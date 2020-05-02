d7971f0b2dacfa576ba0d52e815b29e862cc09f9
Shivam Baghel <shivamunlimited@gmail.com>
2020-05-03 01:07:43 +0530

    Update ImageScrapper.py

--- src/ImageScrapper.py
+++ src/ImageScrapper.py
@@ -7,7 +7,7 @@ r=requests.get(url)
 data=r.json()
 # Storing Names
 pokemonNames=[]
-for i in data['results']:
+for i in data['results'][0:10]:
     pokemonNames.append(i['name'])
 pokemonImagePageUrls=[]
 id=1
@@ -20,13 +20,18 @@ for i in pokemonNames:
     id=id+1
 
 # Now Making requests
+# Also finding which are exceptional URLs
+expectional=[]
+id=0
 for url in pokemonImagePageUrls[0:2]:
     page = requests.get(url)
-    print(type(page.status_code),page.status_code)
-    soup=BeautifulSoup(page.content, 'html.parser')
-    res=soup.find(class_='fullMedia').find(class_="internal")
-    print(res['href'])
-
+    if(page.status_code==200):
+        soup=BeautifulSoup(page.content, 'html.parser')
+        res=soup.find(class_='fullMedia').find(class_="internal")
+        print(res['href'])
+    else:
+        exceptional.append(pokemonNames[id])
+print(exceptional)
 #myurls="https://bulbapedia.bulbagarden.net/wiki/File:154Meganium.png"
 ##mydivs = soup.findAll("div", {"class": "fullMedia"})
 #links = soup.findAll("a", class_="fullMedia internal")
