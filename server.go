b11966bc93515acb1524807094a4066bd1b25370
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-08-18 21:02:34 -0700

    add better .env management

--- server.go
+++ server.go
@@ -4,18 +4,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+
 	fs := http.FileServer(http.Dir("./assets"))
-	http.Handle("/", fs)
 	port := "8080"
 	if os.Getenv("PORT") != "" {
 		port = os.Getenv("PORT")
 	}
 	log.Print("Listening on "+port)
 
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, fs)
 	if err != nil {
 		log.Fatal(err)
 	}
